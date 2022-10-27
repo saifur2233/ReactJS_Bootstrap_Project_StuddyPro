55b3267a7887b05a7ec6347b7dde8f08130e91a0
saifur2233 <saifurnstuiit223344@gmail.com>
2022-10-27 08:04:48 +0600

    faq added

--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -4,17 +4,60 @@ import Accordion from 'react-bootstrap/Accordion';
 const FAQ = () => {
   return (
     <div className='container my-5'>
+      <div className='mb-4 text-center'>
+        <h2 className='text-primary'>Frequently Asked Questions</h2>
+        <p>Ut eget mattis lacus, sit amet accumsan erat. Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+        </p>
+      </div>
         <Accordion defaultActiveKey="0" className='border border-white'>
       <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Header>How do I register and join Studdy?</Accordion.Header>
         <Accordion.Body>
-      
+        <p>
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          </p>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header>What are the mainstay courses at Studdy?</Accordion.Header>
         <Accordion.Body>
-
+        <p>
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          </p>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>How do I register and join Studdy?</Accordion.Header>
+        <Accordion.Body>
+        <p>
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          </p>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>What payment methods are available?</Accordion.Header>
+        <Accordion.Body>
+        <p>
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          </p>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="4">
+        <Accordion.Header>Can I select multiple courses?</Accordion.Header>
+        <Accordion.Body>
+          <p>
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          Integer suscipit justo vel iaculis scelerisque. Nam vel porta augue. Proin egestas leo magna, vel tincidunt magna laoreet eu.
+          </p>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
